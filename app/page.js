a3507ffaa3b9ad8ff0ee5e87dd3624f82fb3bff9
Jeffrey Daniel <78628314+jdaniel6@users.noreply.github.com>
2025-03-12 17:52:17 -0500

    adds skeleton
    adds the skeleton for where the privacy policy and terms of service will go

--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h1>hello</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/terms-of-service">Terms of Service</Link>
+          </li>
+          <li>
+            <Link href="/privacy-policy">Privacy Policy</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
---
+++ app/terms-of-service.js
@@ -0,0 +1,8 @@
+export default function TermsOfService() {
+    return (
+        <div>
+            <h1>Terms of Service</h1>
+            <p>Your terms of service content goes here.</p>
+        </div>
+    );
+}
